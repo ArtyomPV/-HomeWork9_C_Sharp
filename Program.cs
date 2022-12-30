851bd8a29e4b323edf5a87ad302d5225e786f383
Artyom <itcoding76@gmail.com>
2022-12-31 00:01:59 +0200

    Taask 1 is solved

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿/*
+Задача 64: Написать рекурсивную функцию перевода 
+из десятичной системы исчисления в двоичную.
+*/
+//====================== task 1 ========================
+
+ void ConvertDecToBin(int n){
+    string binNumber = string.Empty;
+    if(n!=0) ConvertDecToBin(n/2);
+    binNumber+=n%2;
+    Console.Write(binNumber);
+     }
+Console.Write("Enter a decimal number: ");
+int decNumber = Convert.ToInt32(Console.ReadLine());
+ConvertDecToBin(decNumber);
+/*
+Задача 66: Задайте значения M и N.
+ Напишите программу, которая найдёт сумму
+  натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+//====================== task 1 ========================
